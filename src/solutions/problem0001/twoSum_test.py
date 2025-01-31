08035e7b7e44296925632e77056884cdf8c867ac
shilin83 <shilin_83@qq.com>
2025-01-31 21:03:32 +0800

    1.两数之和

---
+++ src/solutions/problem0001/twoSum_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from typing import List
+from twoSum import Solution
+
+testdata = [
+    ([2, 7, 11, 15], 9, [0, 1]),
+    ([3, 2, 4], 6, [1, 2]),
+    ([3, 3], 6, [0, 1]),
+    ([1, 2], 4, []),
+]
+
+
+@pytest.mark.parametrize("nums, target, expected", testdata)
+def test_twoSum(nums: List[int], target: int, expected: List[int]) -> None:
+    solution = Solution()
+
+    assert solution.twoSum(nums, target) == expected
